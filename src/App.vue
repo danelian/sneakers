593cefcde8821b3fa0ff9b5994aa79f4b0f7d5eb
danelian <141317269+danelian@users.noreply.github.com>
2023-12-16 01:03:27 +0300

    Component - Card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
   <div class="bg-white w-full m-auto rounded-xl shadow-xl shadow-grey-200 mt-20">
     <TheHeader />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-4">Все кроссовки</h2>
+
+      <CardList />
+    </div>
   </div>
 </template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from '../components/Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавить!')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-2 gap-10">
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :is-favorite="true"
+      :onClickAdd="onClickAdd"
+    />
+    <Card imageUrl="/sneakers/sneakers-2.jpg" />
+    <Card imageUrl="/sneakers/sneakers-3.jpg" />
+    <Card imageUrl="/sneakers/sneakers-4.jpg" />
+    <Card imageUrl="/sneakers/sneakers-5.jpg" />
+  </div>
+</template>
