cb60b9af2041f1883ae6d254dd6592c37dc6e3c0
danelian <141317269+danelian@users.noreply.github.com>
2023-12-16 02:47:09 +0300

    filters (select and search input)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios';
 
 import TheHeader from './components/TheHeader.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
 
-const items = ref([]) // { value: [] }
+const items = ref([])
 
-onMounted(async () => {
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+});
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
-    const { data } = await axios.get('https://604781a0efa572c1.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(
+      `https://604781a0efa572c1.mokky.dev/items`, {
+        params
+      })
     items.value = data;
   } catch (err) {
     console.log(err)
   }
-});
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -28,15 +55,17 @@ onMounted(async () => {
       <div class="flex justify-between">
         <h2 class="text-3xl font-bold mb-4">Все кроссовки</h2>
         <div class="flex items-center gap-4">
-          <select
+          <select 
+            @change="onChangeSelect"
             class="py-2 px-3 border border-gray-200 focus:border-gray-400 rounded-md focus:outline-none"
           >
             <option value="name">По названию</option>
             <option value="price">По цене (дешевые)</option>
-            <option value="price">По цене (дорогие)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
           <div class="relative">
             <input
+              @input="onChangeSearchInput"
               type="text"
               class="border border-gray-200 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:border-gray-400"
               placeholder="Поиск..."
