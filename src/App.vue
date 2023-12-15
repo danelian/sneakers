9b32a5fd648cefe960baba6db8fd30f822121227
danelian <141317269+danelian@users.noreply.github.com>
2023-12-16 00:26:48 +0300

    Component - TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <h1 class="text-5xl underline text-green-600">Vue Sneakers</h1>
+  <div class="bg-white w-full m-auto rounded-xl shadow-xl shadow-grey-200 mt-20">
+    <TheHeader />
+  </div>
 </template>
